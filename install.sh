cb3a265edf4e4d3483b66d2971dcab7a1a0c5127
Your Name <you@example.com>
2025-03-23 13:05:47 +0000

    Fixed issue that came along with pytorch or buildtools update

--- install.sh
+++ install.sh
@@ -11,10 +11,16 @@ source .venv/bin/activate
 python -m pip install opencv-python
 
 # Install sam2 into venv
+if [ -d "sam2" ]; then
+    rm -rf sam2
+fi
+
 git clone https://github.com/facebookresearch/sam2.git
 cd sam2
 # If you get an error about filesystem running out of space, then uncomment the following line
 # export TMPDIR='/var/tmp'
+
+sed -i '3s/"setuptools>=61.0",/"setuptools>=62.3.0,<75.9",/' pyproject.toml # Fix setuptools version https://github.com/facebookresearch/sam2/issues/611#issuecomment-2741690522
 python -m pip install -e .
 
 # Download checkpoints for sam2
